083aa3a3d6da08b85332424406b4294ca1ea0526
Çağrı Demirtaş <cagridemirtash@gmail.com>
2023-04-06 01:38:25 +0300

    Codu bir şekilde yaptık ama nasıl yaptık

--- our-project/MyProject/Main.cpp
+++ our-project/MyProject/Main.cpp
@@ -1,15 +1,115 @@
-#include"icb_gui.h"
-int MLE; //Çok Satýrlý metin düzenleyici penceresi kulpu (handle)
-void ButonFonksiyonu(); //Buton fonksiyonu deklare edildi
-void ICGUI_Create()//Ana pencere yaratýldý
-{ }
-void ICGUI_main()//C++ olmazsa olmazý main
-{
-	ICG_Button(5, 5, 120, 25, "BUTON", ButonFonksiyonu);
-	MLE = ICG_MLEditSunken(5, 80, 400, 400, "", SCROLLBAR_V);
+#include <iostream>
+#include "icb_gui.h"
+#include <chrono>
+#include <thread>
+void ICGUI_Create() {
+
 }
-void ButonFonksiyonu()//Buton’a basýnca olacaklar
+
+void ICGUI_main()
 {
-	for (int x = 1; x < 11; x++)
-		ICG_printf(MLE, "%d-Merhaba Dünya!\n");
+
+}
+
+#include <iostream>
+
+class SevenSegmentDisplay {
+public:
+    SevenSegmentDisplay() {
+        for (int i = 0; i < 7; i++) {
+            segments[i] = ' ';
+        }
+    }
+    void setNumber(int number) {
+        switch (number) {
+        case 0:
+            segments[0] = segments[1] = segments[2] = segments[4] = segments[5] = segments[6] = true;
+            break;
+        case 1:
+            segments[1] = segments[2] = true;
+            break;
+        case 2:
+            segments[0] = segments[1] = segments[3] = segments[4] = segments[6] = true;
+            break;
+        case 3:
+            segments[0] = segments[1] = segments[2] = segments[3] = segments[6] = true;
+            break;
+        case 4:
+            segments[1] = segments[2] = segments[5] = segments[6] = true;
+            break;
+        case 5:
+            segments[0] = segments[2] = segments[3] = segments[5] = segments[6] = true;
+            break;
+        case 6:
+            segments[0] = segments[2] = segments[3] = segments[4] = segments[5] = segments[6] = true;
+            break;
+        case 7:
+            segments[0] = segments[1] = segments[2] = true;
+            break;
+        case 8:
+            segments[0] = segments[1] = segments[2] = segments[3] = segments[4] = segments[5] = segments[6] = true;
+            break;
+        case 9:
+            segments[0] = segments[1] = segments[2] = segments[5] = segments[6] = true;
+            break;
+        }
+    }
+
+    void clear() {
+        for (int i = 0; i < 7; i++) {
+            segments[i] = ' ';
+        }
+    }
+    void display() {
+        for (int i = 0; i < 7; i++) {
+            if (segments[i]) {
+                std::cout << '#';
+            }
+            else {
+                std::cout << ' ';
+            }
+            if (i == 2 || i == 4) {
+                std::cout << '\n';
+            }
+        }
+        std::cout << '\n';
+    }
+private:
+    bool segments[7];
+};
+
+int main() {
+    SevenSegmentDisplay display;
+
+    while (true) {
+        time_t now = time(nullptr);
+        struct tm* timeinfo = localtime(&now);
+
+        int hour = timeinfo->tm_hour;
+        int minute = timeinfo->tm_min;
+
+        display.clear();
+        display.setNumber(hour / 10);
+        display.display();
+
+        display.clear();
+        display.setNumber(hour % 10);
+        display.display();
+
+        display.clear();
+        display.setNumber(10); // iki nokta için
+        display.display();
+
+        display.clear();
+        display.setNumber(minute / 10);
+        display.display();
+
+        display.clear();
+        display.setNumber(minute % 10);
+        display.display();
+
+        break;
+    }
+
+    return 0;
 }
