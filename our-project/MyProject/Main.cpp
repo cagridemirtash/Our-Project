a7f8508b73f3ec64eee925420fc3de764ba49099
Çağrı Demirtaş <cagridemirtash@gmail.com>
2023-04-08 22:03:26 +0300

    Çizdirdik bişiler

--- our-project/MyProject/Main.cpp
+++ our-project/MyProject/Main.cpp
@@ -1,115 +1,77 @@
 #include <iostream>
 #include "icb_gui.h"
-#include <chrono>
-#include <thread>
+#include <vector>
+int MLE;
+void OnClickButton();
+void SevenSegmentDisplay(HWND hwnd, int x, int y, int segmentSize, int value);
+void DrawSegment(HDC hdc, int x, int y, int width, int height);
+void CustomPaintFunction(HWND hwnd);
+
 void ICGUI_Create() {
 
 }
 
 void ICGUI_main()
 {
-
+    ICG_MWSize(400, 300);
+    ICG_MWPosition(100, 100);
+    ICG_MWTitle(TEXT("Seven-Segment Display Example"));
+    MLE = ICG_MLEditSunken(5, 80, 400, 400, "", SCROLLBAR_V);
+    ICG_Button(150, 50, 100, 30, "Update",OnClickButton);
 }
 
-#include <iostream>
+void OnClickButton() 
+{
+    CustomPaintFunction(ICG_GetHWND(MLE));
+}
 
-class SevenSegmentDisplay {
-public:
-    SevenSegmentDisplay() {
-        for (int i = 0; i < 7; i++) {
-            segments[i] = ' ';
-        }
-    }
-    void setNumber(int number) {
-        switch (number) {
-        case 0:
-            segments[0] = segments[1] = segments[2] = segments[4] = segments[5] = segments[6] = true;
-            break;
-        case 1:
-            segments[1] = segments[2] = true;
-            break;
-        case 2:
-            segments[0] = segments[1] = segments[3] = segments[4] = segments[6] = true;
-            break;
-        case 3:
-            segments[0] = segments[1] = segments[2] = segments[3] = segments[6] = true;
-            break;
-        case 4:
-            segments[1] = segments[2] = segments[5] = segments[6] = true;
-            break;
-        case 5:
-            segments[0] = segments[2] = segments[3] = segments[5] = segments[6] = true;
-            break;
-        case 6:
-            segments[0] = segments[2] = segments[3] = segments[4] = segments[5] = segments[6] = true;
-            break;
-        case 7:
-            segments[0] = segments[1] = segments[2] = true;
-            break;
-        case 8:
-            segments[0] = segments[1] = segments[2] = segments[3] = segments[4] = segments[5] = segments[6] = true;
-            break;
-        case 9:
-            segments[0] = segments[1] = segments[2] = segments[5] = segments[6] = true;
-            break;
-        }
-    }
+void SevenSegmentDisplay(HWND hwnd, int x, int y, int segmentSize, int value)
+{
+    HDC hdc = GetDC(hwnd);
+    SetBkMode(hdc, TRANSPARENT);
+    HPEN hPen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 0));
+    SelectObject(hdc, hPen);
+    SelectObject(hdc, hBrush);
 
-    void clear() {
-        for (int i = 0; i < 7; i++) {
-            segments[i] = ' ';
-        }
-    }
-    void display() {
-        for (int i = 0; i < 7; i++) {
-            if (segments[i]) {
-                std::cout << '#';
-            }
-            else {
-                std::cout << ' ';
-            }
-            if (i == 2 || i == 4) {
-                std::cout << '\n';
-            }
-        }
-        std::cout << '\n';
-    }
-private:
-    bool segments[7];
-};
+    int segmentWidth = segmentSize;
+    int segmentHeight = segmentSize / 4;
+    int gap = segmentHeight / 2;
 
-int main() {
-    SevenSegmentDisplay display;
+    std::vector<std::vector<int>> segmentValues = {
+        {1, 1, 1, 0, 1, 1, 1}, // 0
+        {0, 1, 1, 0, 0, 0, 0}, // 1
+        {1, 1, 0, 1, 1, 0, 1}, // 2
+        {1, 1, 1, 1, 0, 0, 1}, // 3
+        {0, 1, 1, 1, 0, 1, 0}, // 4
+        {1, 0, 1, 1, 0, 1, 1}, // 5
+        {1, 0, 1, 1, 1, 1, 1}, // 6
+        {1, 1, 1, 0, 0, 0, 0}, // 7
+        {1, 1, 1, 1, 1, 1, 1}, // 8
+        {1, 1, 1, 1, 0, 1, 1}  // 9
+    };
 
-    while (true) {
-        time_t now = time(nullptr);
-        struct tm* timeinfo = localtime(&now);
+    std::vector<int> segmentsToDraw = segmentValues[value];
 
-        int hour = timeinfo->tm_hour;
-        int minute = timeinfo->tm_min;
+    // Draw segments
+    if (segmentsToDraw[0]) DrawSegment(hdc, x + gap, y, segmentWidth - 2 * gap, segmentHeight); // A
+    if (segmentsToDraw[1]) DrawSegment(hdc, x + segmentWidth - segmentHeight, y + gap, segmentHeight, segmentWidth - gap); // B
+    if (segmentsToDraw[2]) DrawSegment(hdc, x + segmentWidth - segmentHeight, y + segmentWidth + gap, segmentHeight, segmentWidth - gap); // C
+    if (segmentsToDraw[3]) DrawSegment(hdc, x + gap, y + 2 * segmentWidth, segmentWidth - 2 * gap, segmentHeight); // D
+    if (segmentsToDraw[4]) DrawSegment(hdc, x, y + segmentWidth + gap, segmentHeight, segmentWidth - gap); // E
+    if (segmentsToDraw[5]) DrawSegment(hdc, x, y + gap, segmentHeight, segmentWidth - gap); // F
+    if (segmentsToDraw[6]) DrawSegment(hdc, x + gap, y + segmentWidth, segmentWidth - 2 * gap, segmentHeight); // G
 
-        display.clear();
-        display.setNumber(hour / 10);
-        display.display();
+    // Clean up GDI objects
+    DeleteObject(hBrush);
+    DeleteObject(hPen);
+    ReleaseDC(hwnd, hdc);
+}
 
-        display.clear();
-        display.setNumber(hour % 10);
-        display.display();
+void DrawSegment(HDC hdc, int x, int y, int width, int height) {
+    Rectangle(hdc, x, y, x + width, y + height);
+}
 
-        display.clear();
-        display.setNumber(10); // iki nokta için
-        display.display();
-
-        display.clear();
-        display.setNumber(minute / 10);
-        display.display();
-
-        display.clear();
-        display.setNumber(minute % 10);
-        display.display();
-
-        break;
-    }
-
-    return 0;
+void CustomPaintFunction(HWND hwnd) {
+    SevenSegmentDisplay(hwnd, 200, 200, 100, 8);
 }
